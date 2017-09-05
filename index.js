7b4848216ee5432976ebd95b80fc841104a3e1d1
Mark Sarcone <mdsarcone@gmail.com>
2017-09-05 01:04:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-
+  var newObj = Object.assign({}, object)
+  
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
